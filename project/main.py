e056913ba349a8bf0bafbe92541d06a22c242254
er1csu <ekeen4@gmail.com>
2016-04-13 00:57:45 -0500

    Receive HTTP 200 response

--- project/main.py
+++ project/main.py
@@ -1,9 +1,49 @@
-import sys
 import requests
+import sys
+import tweepy
+import urllib
+
+class TwitterAPI():
+
+	def __init__(self): 
+		print "Hello"
+		consumer_key = "97GYp45AVZHBbLO2Ku80FKSoI"
+		consumer_secret = "GUOrCYJCE1WosX3hlB1VdNkzGrBoks1Dzz824Zk8DZega3whMf"
+		auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+		access_token = "2956209245-ozCODTOUW9wz2kFYd3RdJxI8qP1trfoISqPPjHr"
+		access_token_secret = "XwfpFr0vptElH47qWDJLKLABWxL0xVdfdtMQPVSlx0ud4"
+		auth.set_access_token(access_token, access_token_secret)
+		self.api = tweepy.API(auth)
+
+	def test_tweet(self, message): 
+		self.api.update_status(status=message)
+
+
+class PocketAPI():
+
+	def __init__(self): 
+		self.consumer_key = "53266-0352132bef521461e927c48e"
+		self.request_token = ""
+		self.access_token = ""
+		self.redirect_uri = "https://www.google.com"
+		
 
 def main(): 
-	"""Main entry point"""
+	print "Hello world"
+	twitter = TwitterAPI()
+	pocket = PocketAPI()
 
+	consumer_key = pocket.consumer_key
+	url = pocket.redirect_uri
+	payload = {'redirect_uri': url, 'consumer_key': consumer_key}
+	r = requests.post('https://getpocket.com/v3/oauth/request', json=payload)
+	
 
-if pass __name__ == '__main__': 
-	sys.exit(main())
+	redirect_url = "https://getpocket.com/auth/authorize?"
+	request_payload = {'request_token': }
+	urllib.urlencode(redirect_url, )
+	webbrowser.open_new(redirect_url)
+
+if __name__ == '__main__':
+    sys.exit(main())
+	
